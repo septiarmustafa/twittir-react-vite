71b3e73a1b72e8146b379ba5441768816dbca8ed
septiar <septiarmustafa@gmail.com>
2024-01-21 15:31:34 +0700

    fixing toggle

--- src/components/TweetCard.jsx
+++ src/components/TweetCard.jsx
@@ -52,10 +52,7 @@ const TweetCard = ({ post }) => {
                           viewBox="0 0 16 16"
                           onClick={handleLoveToggle}
                         >
-                          <path
-                            fill-rule="evenodd"
-                            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
-                          />
+                          <path d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
                         </svg>
                       )}
                     </div>
